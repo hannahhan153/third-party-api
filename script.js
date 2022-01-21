2675a05184d685316235b6f5544156774f84f474
hannahhan153 <hannah.han@biola.edu>
2022-01-21 08:31:41 -0800

    pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// 
+// when user opens planner, current day is displayed at top of calendar
+
+// when user scrolls down, time blocks for standard business hours displayed
+
+// when user views time blocks for the day, each time block is color-coded to indicate past, present, or future
+
+// when user clicks into a time block, user can enter an event
+
+// when user clicks save button for time block, text for event is saved in local storage
+
+// when user refresh page, saved events persist
