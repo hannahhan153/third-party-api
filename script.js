0528c66880805a8202c9dbba29a82621f4df263d
hannahhan153 <hannah.han@biola.edu>
2022-01-22 15:13:12 -0800

    past,present,future color-code

--- script.js
+++ script.js
@@ -14,18 +14,19 @@ $(".saveBtn").on("click", function () {
 })
 
 // when user refresh page, saved events persist
-$("#hour-8 .description").val(localStorage.getItem("hour-8"))
-$("#hour-9 .description").val(localStorage.getItem("hour-9"))
-$("#hour-10 .description").val(localStorage.getItem("hour-10"))
-$("#hour-11 .description").val(localStorage.getItem("hour-11"))
-$("#hour-12 .description").val(localStorage.getItem("hour-12"))
-$("#hour-13 .description").val(localStorage.getItem("hour-13"))
-$("#hour-14 .description").val(localStorage.getItem("hour-14"))
-$("#hour-15 .description").val(localStorage.getItem("hour-15"))
-$("#hour-16 .description").val(localStorage.getItem("hour-16"))
-$("#hour-17 .description").val(localStorage.getItem("hour-17"))
-$("#hour-18 .description").val(localStorage.getItem("hour-18"))
-$("#hour-19 .description").val(localStorage.getItem("hour-19"))
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-18 .description").val(localStorage.getItem("hour-18"));
+$("#hour-19 .description").val(localStorage.getItem("hour-19"));
+
 
 // when user views time blocks for the day, each time block is color-coded to indicate past, present, or future
 function hourUpdater() {
